0208daa7eb9dfedd66b7d10319f773f06480b4bd
DipaliBarapatre <dipalibarapatre@gmail.com>
2025-04-29 21:13:48 +0530

    Updated for delimeter

--- string_calculator.rb
+++ string_calculator.rb
@@ -6,5 +6,19 @@ class StringCalculator
 
 		return numbers if numbers.is_a?(Integer)
 
+	    #Find delimeters
+		delimeter, numbers_list = get_delimeter(numbers)
+
+	end
+
+	private
+
+	def get_delimeter(numbers)
+		if numbers.start_with?('//')
+		   delimiter, numbers = numbers.split("\n", 2)
+           delimiter = delimiter[2..-1]
+           numbers = numbers.gsub(delimiter, ",")
+        end
+        return delimiter, numbers
 	end
 end
