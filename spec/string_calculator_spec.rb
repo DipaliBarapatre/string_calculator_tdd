89cf9f71d3ef05bedac1eceead442f160c33532d
DipaliBarapatre <dipalibarapatre@gmail.com>
2025-04-29 20:56:30 +0530

    Updated rspec for sum of numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -7,4 +7,12 @@ RSpec.describe StringCalculator do
      expect(calculator.add("")).to eq(0)
    end
 
+    it "returns the number for a single number" do
+      expect(calculator.add("3")).to eq(3)
+    end
+
+    it "returns sum of two numbers separated by a comma" do
+      expect(calculator.add("3,2")).to eq(5)
+    end
+
 end
